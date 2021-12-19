5f1caa1cd1928876f46bd791e9944fdcda7b6490
chon26909 <59905927+chon26909@users.noreply.github.com>
2021-12-19 21:59:42 +0700

    update app

--- app.js
+++ app.js
@@ -2,14 +2,12 @@ const express = require('express');
 const app = express();
 const cors = require('cors')
 
-require('dotenv').config();
-require('./config/database').connect();
+// require('dotenv').config();
+// require('./config/database').connect();
 
-app.use(cors());
-app.use(express.json());
+// app.use(cors());
+// app.use(express.json());
 
-app.get('/', (req, res) => { 
-    res.send('Hello World')
-})
+// app.use('/api/post', require('./routes/postRoute.route'));       
 
 module.exports = app;
