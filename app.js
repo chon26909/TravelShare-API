7026e8a9134dd1b76898c7a3d6d91b9e9bcb08ae
chon26909 <59905927+chon26909@users.noreply.github.com>
2021-12-19 22:01:49 +0700

    use cors

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 require('dotenv').config();
 require('./config/database').connect();
 
-// app.use(cors());
+app.use(cors());
 // app.use(express.json());
 
 // app.use('/api/post', require('./routes/postRoute.route'));       
