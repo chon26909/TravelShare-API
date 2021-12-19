57dd271d2dabde07037eaada16303bb066b62007
chon26909 <59905927+chon26909@users.noreply.github.com>
2021-12-19 22:00:44 +0700

    create get all post

---
+++ controllers/postController.js
@@ -0,0 +1,23 @@
+
+const Post = require('../models/Post.model');
+
+const getAllPost = async (req, res) => {
+    try {
+        const posts = await Post.find();
+    } catch (error) {
+        console.log('error get post', error)
+        res.json({ message: 'note found in database'});
+    }
+}
+
+const addPost = async = () => { 
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+module.exports = { 
+    getAllPost
+}
