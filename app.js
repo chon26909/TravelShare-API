0a3348edec61746d9826ec5ade2e9b91970f038e
chon26909 <59905927+chon26909@users.noreply.github.com>
2021-12-19 22:00:27 +0700

    use dotenv

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors')
 
-// require('dotenv').config();
+require('dotenv').config();
 // require('./config/database').connect();
 
 // app.use(cors());
